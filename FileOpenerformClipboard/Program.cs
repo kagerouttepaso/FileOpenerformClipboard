39254ef054bc877345e3b0ecb7c5bfe08f06ef49
kitsunai <kitsunai@sanritz.co.jp>
2014-03-13 16:20:00 +0900

    再帰検索

--- FileOpenerformClipboard/Program.cs
+++ FileOpenerformClipboard/Program.cs
@@ -25,30 +25,45 @@ namespace FileOpenerformClipboard
 			} else
 				return;
 
-			//選択された文字列を\をつけて結合。
-			var url = String.Join("\\",
-				//改行コード変更後
-						str.Replace("\r", "")
-				//各行頭の空白文字とおせっかいな行末の\を削除
-						.Split('\n')
-						.Select(s => Regex.Replace(s.Trim(), @"^(.*?)[\\]?$", "$1"))
-						.ToArray()
-					);
+            var lines = str
+                //改行コード変更後
+                .Replace("\r", "")
+                .Split('\n')
+                //各行頭の空白文字とおせっかいな行末の\を削除
+                .Select(x => Regex.Replace(x.Trim(), @"^(.*?)[\\]?$", "$1"))
+                .ToList();
+            Action<int,string> serch = (c,s) => { };
+            var list = new List<string>();
+            serch = (count, name) =>
+            {
+                if (lines.Count() <= count) { return; }
+                else if (lines.Count() - 1 == count)
+                {
+                    list.Add(string.Format("{0}\\{1}", name, lines[count]));
+                    list.Add(string.Format("{0}{1}", name, lines[count]));
+                }
+                else
+                {
+                    if (count != 0) serch(count + 1, string.Format("{0}\\{1}", name, lines[count]));
+                    serch(count + 1, string.Format("{0}{1}", name, lines[count]));
+                    list.Add(string.Format("{0}\\{1}", name, lines[count]));
+                    list.Add(string.Format("{0}{1}", name, lines[count]));
+
+                }
+            };
+            serch(0, "");
+
+
 			//作った文字列がフォルダかファイルのパスならば、ファイルオープン
-			if(Directory.Exists(url) || File.Exists(url)) {
-#if DEBUG
-
-				Console.WriteLine( str );
-				Console.ReadKey();
-#else
-				System.Diagnostics.Process.Start(url);
-#endif
-			}
-
+            if (list.Any(uri => (Directory.Exists(uri) || File.Exists(uri))))
+            {
+                var filename = list.First(uri => (Directory.Exists(uri) || File.Exists(uri)));
+                System.Diagnostics.Process.Start(filename);
+            }
 
 			//おまけ 選択された文字列の中にURLがあったら開いてみる
 			if(str.Replace("\r", "").Split('\n').Any(x => Regex.IsMatch(x, @"^[\s]*(http|https|ftp)://.*"))) {
-				url = str.Split('\n')
+				var url = str.Split('\n')
 					.First(x => Regex.IsMatch(x, @"^[\s]*(http|https|ftp)://.*"));
 				System.Diagnostics.Process.Start(url);
 			}
