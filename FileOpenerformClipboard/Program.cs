529ad5efa534979cbdd360b9a3fb378b2e4fe0bb
kitsunai <kitsunai@sanritz.co.jp>
2014-03-18 18:30:57 +0900

    アルゴリズム効率化

--- FileOpenerformClipboard/Program.cs
+++ FileOpenerformClipboard/Program.cs
@@ -15,16 +15,19 @@ namespace FileOpenerformClipboard
 {
 	class Program
 	{
-		[STAThreadAttribute]
-		static void Main(string[] args) {
-			//クリップボードから取得
-			var clipboard_object = Clipboard.GetDataObject();
-			//文字列の取得、出来なかったら終了
-			var str = "";
-			if(clipboard_object.GetDataPresent(DataFormats.Text)) {
-				str = (String)clipboard_object.GetData(DataFormats.Text);
-			} else
-				return;
+        [STAThreadAttribute]
+        static void Main(string[] args)
+        {
+            //クリップボードから取得
+            var clipboard_object = Clipboard.GetDataObject();
+            //文字列の取得、出来なかったら終了
+            var str = "";
+            if (clipboard_object.GetDataPresent(DataFormats.Text))
+            {
+                str = (String)clipboard_object.GetData(DataFormats.Text);
+            }
+            else
+                return;
 
             var lines = str
                 //改行コード変更後
@@ -32,47 +35,51 @@ namespace FileOpenerformClipboard
                 .Split('\n')
                 //各行頭の空白文字とおせっかいな行末の\を削除
                 .Select(x => Regex.Replace(x.Trim(), @"^(.*?)[\\]?$", "$1"))
+                //空行を削除
+                .Where(x => x != "")
                 .ToList();
-            Action<int,string> serch = (c,s) => { };
+
+            Action<int, string> serch = (c, s) => { };
             var list = new List<string>();
             serch = (count, name) =>
             {
                 if (lines.Count() <= count) { return; }
                 else if (lines.Count() - 1 == count)
                 {
-                    list.Add(string.Format("{0}\\{1}", name, lines[count]));
+                    if (name != "") list.Add(string.Format("{0}\\{1}", name, lines[count]));
                     list.Add(string.Format("{0}{1}", name, lines[count]));
                 }
                 else
                 {
-                    if (count != 0) serch(count + 1, string.Format("{0}\\{1}", name, lines[count]));
+                    if (name != "") serch(count + 1, string.Format("{0}\\{1}", name, lines[count]));
                     serch(count + 1, string.Format("{0}{1}", name, lines[count]));
                     list.Add(string.Format("{0}\\{1}", name, lines[count]));
                     list.Add(string.Format("{0}{1}", name, lines[count]));
-
                 }
             };
-            (lines.Count - 1).Takes(i =>
+            (lines.Count).Takes(i =>
             {
                 serch(i, "");
             });
 
 
-			//作った文字列がフォルダかファイルのパスならば、ファイルオープン
-            if (list.Any(uri => (Directory.Exists(uri) || File.Exists(uri))))
+            //作った文字列がフォルダかファイルのパスならば、ファイルオープン
+            var filename = list
+                .Where(x => !string.IsNullOrWhiteSpace(x) && x != "\\" && !Regex.IsMatch(x, @"^[\\][\\][\\]"))
+                .Where(uri => (Directory.Exists(uri) || File.Exists(uri))).ToList();
+            if (filename.Count() > 0)
             {
-                var filename = list.Where(x => !string.IsNullOrWhiteSpace(x) && x != "\\" && !Regex.IsMatch(x, @"^[\\][\\][\\]"))
-                    .Where(uri => (Directory.Exists(uri) || File.Exists(uri)));
-                System.Diagnostics.Process.Start(filename.OrderBy(x=>x).First());
+                System.Diagnostics.Process.Start(filename.OrderByDescending(x => x.Length).First());
             }
 
-			//おまけ 選択された文字列の中にURLがあったら開いてみる
-			if(str.Replace("\r", "").Split('\n').Any(x => Regex.IsMatch(x, @"^[\s]*(http|https|ftp)://.*"))) {
-				var url = str.Split('\n')
-					.First(x => Regex.IsMatch(x, @"^[\s]*(http|https|ftp)://.*"));
-				System.Diagnostics.Process.Start(url);
-			}
+            //おまけ 選択された文字列の中にURLがあったら開いてみる
+            if (str.Replace("\r", "").Split('\n').Any(x => Regex.IsMatch(x, @"^[\s]*(http|https|ftp)://.*")))
+            {
+                var url = str.Split('\n')
+                    .First(x => Regex.IsMatch(x, @"^[\s]*(http|https|ftp)://.*"));
+                System.Diagnostics.Process.Start(url);
+            }
 
-		}
+        }
 	}
 }
