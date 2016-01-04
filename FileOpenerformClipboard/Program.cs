d47fda87d90f3ab29e9f3daeb7ac1891763eb02e
kagerouttepaso <goth.wasawasa@gmail.com>
2016-01-04 13:27:27 +0900

    リファクタリング

--- FileOpenerformClipboard/Program.cs
+++ FileOpenerformClipboard/Program.cs
@@ -1,35 +1,34 @@
-﻿using System;
+﻿//system
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Windows.Forms;
-using System.Windows;
-
-using System.Threading;
-
-using System.Text.RegularExpressions;
 using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+//my Utils
 using Commons.Extensions;
 
 namespace FileOpenerformClipboard
 {
-	class Program
+    class Program
 	{
         [STAThreadAttribute]
         static void Main(string[] args)
         {
             //クリップボードから取得
             var clipboard_object = Clipboard.GetDataObject();
+
             //文字列の取得、出来なかったら終了
-            var str = "";
+            var clipboardString = "";
             if (clipboard_object.GetDataPresent(DataFormats.Text))
             {
-                str = (String)clipboard_object.GetData(DataFormats.Text);
+                clipboardString = (String)clipboard_object.GetData(DataFormats.Text);
             }
             else
                 return;
 
-            var lines = str
+            var clipboardStrings = clipboardString
                 //改行コード変更後
                 .Replace("\r", "")
                 .Split('\n')
@@ -39,32 +38,34 @@ namespace FileOpenerformClipboard
                 .Where(x => x != "")
                 .ToList();
 
+            var tryPathList = new List<string>();
             Action<int, string> serch = (c, s) => { };
-            var list = new List<string>();
             serch = (count, name) =>
             {
-                if (lines.Count() <= count) { return; }
-                else if (lines.Count() - 1 == count)
+                if (clipboardStrings.Count() <= count) { return; }
+                else if (clipboardStrings.Count() - 1 == count)
                 {
-                    if (name != "") list.Add(string.Format("{0}\\{1}", name, lines[count]));
-                    list.Add(string.Format("{0}{1}", name, lines[count]));
+                    if (name != "")
+                        tryPathList.Add(string.Format("{0}\\{1}", name, clipboardStrings[count]));
+                    tryPathList.Add(string.Format("{0}{1}", name, clipboardStrings[count]));
                 }
                 else
                 {
-                    if (name != "") serch(count + 1, string.Format("{0}\\{1}", name, lines[count]));
-                    serch(count + 1, string.Format("{0}{1}", name, lines[count]));
-                    list.Add(string.Format("{0}\\{1}", name, lines[count]));
-                    list.Add(string.Format("{0}{1}", name, lines[count]));
+                    if (name != "")
+                        serch(count + 1, string.Format("{0}\\{1}", name, clipboardStrings[count]));
+                    serch(count + 1, string.Format("{0}{1}", name, clipboardStrings[count]));
+                    tryPathList.Add(string.Format("{0}\\{1}", name, clipboardStrings[count]));
+                    tryPathList.Add(string.Format("{0}{1}", name, clipboardStrings[count]));
                 }
             };
-            (lines.Count).Takes(i =>
+            (clipboardStrings.Count).Takes(i =>
             {
                 serch(i, "");
             });
 
 
             //作った文字列がフォルダかファイルのパスならば、ファイルオープン
-            var filename = list
+            var filename = tryPathList
                 .Where(x => !string.IsNullOrWhiteSpace(x) && x != "\\" && !Regex.IsMatch(x, @"^[\\][\\][\\]"))
                 .Where(uri => (Directory.Exists(uri) || File.Exists(uri))).ToList();
             if (filename.Count() > 0)
@@ -73,9 +74,9 @@ namespace FileOpenerformClipboard
             }
 
             //おまけ 選択された文字列の中にURLがあったら開いてみる
-            if (str.Replace("\r", "").Split('\n').Any(x => Regex.IsMatch(x, @"^[\s]*(http|https|ftp)://.*")))
+            if (clipboardString.Replace("\r", "").Split('\n').Any(x => Regex.IsMatch(x, @"^[\s]*(http|https|ftp)://.*")))
             {
-                var url = str.Split('\n')
+                var url = clipboardString.Split('\n')
                     .First(x => Regex.IsMatch(x, @"^[\s]*(http|https|ftp)://.*"));
                 System.Diagnostics.Process.Start(url);
             }
